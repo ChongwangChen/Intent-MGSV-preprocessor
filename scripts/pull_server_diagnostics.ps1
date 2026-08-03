f6249127b74662a3ef9a8edec43a84904425a5bb
ChenChongwang <zhezhe050923@gmail.com>
2026-08-03 15:34:40 +0800

    Use server environment Python for diagnostics

--- scripts/pull_server_diagnostics.ps1
+++ scripts/pull_server_diagnostics.ps1
@@ -3,13 +3,14 @@ param(
     [int]$Port = 32769,
     [string]$User = "ubuntu",
     [string]$RemoteRoot = "/data/users/ccw/intent_mgsv/repo/MGSV_preprocessor",
+    [string]$RemotePython = "/data/conda/envs/mgsv_data/bin/python",
     [string]$LocalOutput = "E:\MGSV_preprocessor\server_reports\latest.txt",
     [int]$Recent = 20
 )
 
 $ErrorActionPreference = "Stop"
 $remoteReport = "$RemoteRoot/outputs/server/diagnostics/latest.txt"
-$remoteCommand = "cd '$RemoteRoot' && source config/server.env && python -m intent_mgsv_pipeline.server.collect_diagnostics --recent $Recent --out '$remoteReport'"
+$remoteCommand = "cd '$RemoteRoot' && source config/server.env && '$RemotePython' -m intent_mgsv_pipeline.server.collect_diagnostics --recent $Recent --out '$remoteReport'"
 
 Write-Host "Generating a server diagnostic report..."
 & ssh -p $Port "$User@$Server" $remoteCommand
